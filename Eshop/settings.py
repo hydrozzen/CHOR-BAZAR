97a7751fb895f0a2060344492603dc69c028060e
hydrozzen <105576603+hydrozzen@users.noreply.github.com>
2022-07-23 00:57:25 -0700

    Update settings.py

--- Eshop/settings.py
+++ Eshop/settings.py
@@ -122,11 +122,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/uploads/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'uploads')
 #STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'uploads'),
 )
 MEDIA_URL = "/image/download/"
 MEDIA_ROOT = BASE_DIR
